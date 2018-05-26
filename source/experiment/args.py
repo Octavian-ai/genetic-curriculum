ec578dd90b8255a91260ed8e8991dc098e31daba
David Mack <david@sketchdeck.com>
2018-05-26 21:57:54 +0100

    config steps

--- source/experiment/args.py
+++ source/experiment/args.py
@@ -17,8 +17,8 @@ def get_args(args=None):
 	parser.add_argument('--project',				type=str,  default=os.getenv("GOOGLE_CLOUD_PROJECT", "octavian-181621"))
 
 	parser.add_argument('--epochs', 				type=int,  default=1000)
-	parser.add_argument('--micro-step', 			type=int,  default=1000)
-	parser.add_argument('--macro-step', 			type=int,  default=30)
+	parser.add_argument('--micro-step', 			type=int,  default=os.getenv("MICRO_STEP", 1000))
+	parser.add_argument('--macro-step', 			type=int,  default=os.getenv("MACRO_STEP", 30))
 
 	parser.add_argument('--batch-size', 			type=int,  default=32)
 	parser.add_argument('--n-workers', 				type=int,  default=os.getenv("N_WORKERS", 15))
